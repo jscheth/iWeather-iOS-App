d1cb858d15d3929c7243bd5a171b77be04435132
jscheth <sovichea.chet@gmail.com>
2025-04-12 22:30:27 -0700

    Initial Commit

---
+++ iWeatherTests/iWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  iWeatherTests.swift
+//  iWeatherTests
+//
+//  Created by Jonathan Cheth on 4/12/25.
+//
+
+import Testing
+@testable import iWeather
+
+struct iWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
